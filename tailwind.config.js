93290ecec194bf2ed0a3d2f90588661f817239ef
ayushmishra950 <ayush950mishra@gmail.com>
2025-09-20 13:14:19 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html"
   ],
+  safelist: [
+    'm-4', 'p-6', 'md:mr-10', // jaisi classes tumhe ensure karni ho ki purge na ho
+  ],
   theme: {
     extend: {},
   },
